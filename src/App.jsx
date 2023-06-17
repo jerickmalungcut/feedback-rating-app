2b402097c3ce8b0d7b281b78dc92b8bbe12f8ab6
jerickmalungcut <jerickmalungcut@yahoo.com>
2023-06-17 11:29:45 +0800

    added the base vite config

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { v4 as uuidv4 } from "uuid";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { useState } from "react";
 import FeedbackData from "./data/feedbackData";
 import FeedbackList from "./components/FeedbackList";
 import FeedbackStats from "./components/FeedbackStats";
 import FeedbackForm from "./components/FeedbackForm";
 import Header from "./components/Header";
+import AboutPage from "./pages/AboutPage";
 
 function App() {
   const [feedback, setFeedback] = useState(FeedbackData);
@@ -23,14 +25,20 @@ function App() {
   };
 
   return (
-    <>
-      <Header text="Rating Application" />
-      <div className="container py-8 px-2 mx-auto">
-        <FeedbackForm handleAdd={addFeedback} />
-        <FeedbackStats feedback={feedback} />
-        <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
-      </div>
-    </>
+    <Router>
+      <>
+        <Header text="Rating Application" />
+        <div className="container py-8 px-2 mx-auto">
+          <FeedbackForm handleAdd={addFeedback} />
+          <FeedbackStats feedback={feedback} />
+          <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
+
+          <Switch>
+            <Route path="/about" component={AboutPage} />
+          </Switch>
+        </div>
+      </>
+    </Router>
   );
 }
 
